181befcf24eed16f800d5c14c9291aa5c2125cf3
Anson Kee <anson.kee@jobvite-inc.com>
2016-07-08 06:25:39 -0700

    change method name to use underscores for better readability and more in line with ruby coding style

--- selectionSort.rb
+++ selectionSort.rb
@@ -15,7 +15,7 @@ class SelectionSort
 
 	end
 
-	def findminindex(myarray, start_index)
+	def find_min_index(myarray, start_index)
 		# given array and startIndex, iterate through array starting with startindex+1
 		# and find lowest number from entire subarray.
 		# after iteration is done, return index of lowest number 
@@ -44,7 +44,7 @@ class SelectionSort
 			# find lowest number and return its index
 				# if its return swap current index with lowest number index
 		@myarray.each_index do | i |
-			min_index = findminindex(@myarray, i)
+			min_index = find_min_index(@myarray, i)
 			if min_index
 				swap(@myarray, i, min_index)
 
