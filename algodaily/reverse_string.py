ca8fd3d6c7bc1942d3f58536a8bf50121a7f60a6
Anson Kee <anson.kee@jobvite-inc.com>
2021-01-23 11:12:53 -0800

    <JIRA_TICKET> : One line Summary : Why do we need this change?
    =======================
    
    Small description for why the change is needed goes here.
    
    What effects does this change have?
    
    =======================
    
    Effects of the change go here (optional)

--- algodaily/reverse_string.py
+++ algodaily/reverse_string.py
@@ -8,9 +8,6 @@
 # Do not use the built-in #reverse() method or [::-1] if Python
 # Ideal solution would run in O(n) time complexity and O(1) space complexity
 
-
-
-
 def reverse_string(s):
     myarray = list(s)
     reversearray = []
@@ -21,9 +18,25 @@ def reverse_string(s):
         counter -= 1
     return ''.join(reversearray)
 
+# 2nd way that is faster using 2 pointer pattern
+def reverse_string_two_pointer(s):
+    my_array = list(s)
+    start = 0
+    end = len(my_array) - 1
+    while start < end:
+        swap_characters(my_array, start, end)
+        print(my_array)
+        start += 1
+        end -= 1
+    return ''.join(my_array)
 
+def swap_characters(myarray, start, end):
+    temp = myarray[start]
+    myarray[start] = myarray[end]
+    myarray[end] = temp
 
-result = reverse_string('reverseastring')
+#result = reverse_string('reverseastring')
+result = reverse_string_two_pointer('reverseastring')
 print(result)
 
 assert result == 'gnirtsaesrever'
